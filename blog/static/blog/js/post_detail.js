9c12a1d0f33c5b7adb6b82e45e0ede29f87da496
Armin Emami <armin.syntax@gmail.com>
2026-08-18 13:43:38 +0330

    a good commit

--- blog/static/blog/js/post_detail.js
+++ blog/static/blog/js/post_detail.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const postBody = document.getElementById('postBody');
+  const postBody = document.getElementById('postBody');
 
-    if (postBody) {
-        const links = postBody.getElementsByTagName('a');
+  if (postBody) {
+    const links = postBody.getElementsByTagName('a');
 
-        for (let i = 0; i < links.length; i++) {
-            links[i].setAttribute('target', '_blank');
-        }
+    for (let i = 0; i < links.length; i++) {
+      links[i].setAttribute('target', '_blank');
     }
+  }
 });
